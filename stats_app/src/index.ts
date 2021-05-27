6d92553c8555bab9b5fd3aa307a5b423639596fa
DanBilic <bilic90@gmx.de>
2021-05-27 23:08:27 +0200

    added csv file as data source + added MatchResult enum

--- stats_app/src/index.ts
+++ stats_app/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hi");
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+// console.log(matches);
+
+let manUnitedWins = 0;
+enum MatchtResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchtResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchtResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
