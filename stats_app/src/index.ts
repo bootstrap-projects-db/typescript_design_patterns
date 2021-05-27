3d704ad9e2d5be83d9d7d97ff8d70a5d407eb4e3
DanBilic <bilic90@gmx.de>
2021-05-27 23:57:11 +0200

    stats_app: CsvFileReader class added

--- stats_app/src/index.ts
+++ stats_app/src/index.ts
@@ -1,11 +1,7 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", { encoding: "utf-8" })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv");
+reader.read();
 // console.log(matches);
 
 let manUnitedWins = 0;
@@ -15,7 +11,7 @@ enum MatchtResult {
   Draw = "D",
 }
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchtResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchtResult.AwayWin) {
