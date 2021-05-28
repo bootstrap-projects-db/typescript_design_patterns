965018201bd913e7f5fd8f05375a0420782cfba3
DanBilic <bilic90@gmx.de>
2021-05-28 10:20:04 +0200

    stats_app: added tuples to descripe row types

--- stats_app/src/index.ts
+++ stats_app/src/index.ts
@@ -1,20 +1,16 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("football.csv");
 reader.read();
 // console.log(matches);
 
 let manUnitedWins = 0;
-enum MatchtResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
 
 for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchtResult.HomeWin) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchtResult.AwayWin) {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
