92cdf7dd21f77fb1470861cf8020d175938a3c8b
DanBilic <bilic90@gmx.de>
2021-05-28 12:25:07 +0200

    stats_app: compositon approach added

--- stats_app/src/index.ts
+++ stats_app/src/index.ts
@@ -1,13 +1,37 @@
-import { MatchReader } from "./MatchReader";
+// import { MatchReader } from "./inheritance/MatchReader";
+// import { MatchResult } from "./MatchResult";
+
+// const reader = new MatchReader("football.csv");
+// reader.read();
+// // console.log(matches);
+
+// let manUnitedWins = 0;
+
+// for (let match of reader.data) {
+//   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
+
+// console.log(`Man United won ${manUnitedWins} games`);
+
+// compositon approach
+import { MatchReader } from "./composition/MatchReader";
+import { CsvFileReader } from "./composition/CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader("football.csv");
-reader.read();
-// console.log(matches);
+// create an pbject that satisfies the DataReader interface
+const csvFileReader = new CsvFileReader("football.csv");
+
+// create an instance of MachtReader and pass in something
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
--- stats_app/src/inheritance/MatchReader.ts
+++ stats_app/src/inheritance/MatchReader.ts
@@ -1,8 +1,8 @@
 import { CsvFileReader } from "./CsvFileReader";
-import { dateStringToDate } from "./utils";
+import { dateStringToDate } from "../utils";
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-import { MatchResult } from "./MatchResult";
+import { MatchResult } from "../MatchResult";
 
 export class MatchReader extends CsvFileReader<MatchData> {
   mapRow(row: string[]): MatchData {
